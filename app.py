ebc5fe4d3c777d45b2a3b3d25eb35047990e0067
unknown <mzshaikh129@gmail.com>
2021-04-26 17:59:45 +0500

    Added TODO tasks

--- app.py
+++ app.py
@@ -3,4 +3,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def root():
+    #TODO: Add homepage to take input
     return "Hello, There!"
+
+@app.route('/resutles')
+def resutles():
+    #TODO: Add functionality to perform OCR and display resutls
+    return "Lorem ipsum sit amor dit"
