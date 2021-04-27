bd7923c75a12c1d9007239af370c2db3e328a60c
unknown <mzshaikh129@gmail.com>
2021-04-27 13:30:57 +0500

    basic html of the main page added

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def root():
     #TODO: Add homepage to take input
-    return "Hello, There!"
+    #getting url ussing a get request
+    url = request.args.get("url"," ")
+    if url != " ":
+        print("We got the Url")
+
+    return render_template("index.html")
 
 @app.route('/resutles')
 def resutles():
